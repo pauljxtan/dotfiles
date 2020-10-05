1e450758baa5f0b839ed08c2ca3c79c820ba8d34
Paul Tan <pjxtan@gmail.com>
2020-10-04 22:09:12 -0400

    Cleanup

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -6,13 +6,6 @@ PURE_GIT_UNTRACKED_DIRTY=0
 PURE_PROMPT_SYMBOL=λ
 PURE_PROMPT_VICMD_SYMBOL=ψ
 prompt pure
-#zstyle :prompt:pure:path color cyan
-
-#PROMPT_LEAN_VIMODE=1
-#PROMPT_LEAN_COLOR1=blue
-#PROMPT_LEAN_COLOR2=red
-#PROMPT_LEAN_COLOR3=green
-#prompt lean
 
 # Vi mode
 bindkey -v
