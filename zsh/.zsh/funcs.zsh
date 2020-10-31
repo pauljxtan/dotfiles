ae7ff1d29f24305bcd101f84069b01a5b73efe8e
Paul Tan <pjxtan@gmail.com>
2020-10-31 13:47:59 -0400

    Zsh updates (#3)
    Small updates to Zsh config.
    
    - Refactor custom functions to a single file.
    - Add more custom functions (fzf related).
    - Update default fzf search command to include hidden files, which helps for working with files in this repo itself.

---
+++ zsh/.zsh/funcs.zsh
@@ -0,0 +1,69 @@
+#######################################
+# Mount a labelled partition if it is
+# connected and not already mounted.
+#
+# Args:
+#     The partition label
+#     The mount path (if not given,
+#         checks /etc/fstab)
+#######################################
+function try_mount() {
+    local LABEL=$1
+    local MOUNT_PATH=$2
+
+    if [ -b /dev/disk/by-label/$LABEL ]
+    then
+        echo "$LABEL is connected"
+        if [ $(grep -c $LABEL /proc/mounts) -eq 1 ]
+        then
+            echo "  $LABEL is already mounted"
+        else
+            echo "  Mounting $LABEL at $MOUNT_PATH..."
+            sudo mount -L $LABEL $MOUNT_PATH
+        fi
+    else
+        echo "$LABEL is not connected"
+    fi
+}
+
+#######################################
+# Change into a random directory at the
+# given path (non-recursive).
+#
+# Args:
+#     The base path
+#######################################
+function cd_random_subdir() {
+    cd $(ls -d $1/* | shuf | head -n 1)
+}
+
+#######################################
+# Search the command history via fzf
+# and push the selection onto the
+# editing buffer stack.
+#
+# Taken directly from:
+# https://github.com/junegunn/fzf/wiki/
+#        Examples#command-history
+#######################################
+function fh() {
+    print -z $( \
+        ([ -n "$ZSH_NAME" ] && fc -l 1 || history) \
+        | fzf +s --tac \
+        | sed -E 's/ *[0-9]*\*? *//' \
+        | sed -E 's/\\/\\\\/g' \
+    )
+}
+
+#######################################
+# Change into a subdirectory via fzf.
+# Includes hidden directories.
+#
+# Based on:
+# https://github.com/junegunn/fzf/wiki/
+#         Examples#changing-directory
+#######################################
+function fcd() {
+    local DIR
+    DIR=$(fd --type d --hidden | fzf +m) && cd $DIR
+}
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -3,17 +3,15 @@ ZSH_EXTERNAL=$ZSH_BASE/external
 
 fpath+=$ZSH_EXTERNAL/pure
 fpath+=$ZSH_EXTERNAL/lean
-fpath+=$ZSH_BASE/funcs
-
-# TODO put these autoloads somewhere else
-autoload -Uz cd_random_subdir
-autoload -Uz try_mount
 
 source $ZSH_BASE/settings.zsh
 source $ZSH_BASE/prompt.zsh
 source $ZSH_BASE/completion.zsh
+source $ZSH_BASE/funcs.zsh
 source $ZSH_BASE/aliases.zsh
 source $ZSH_BASE/aliases_special.zsh
+
+#### External plugins
 
 source $ZSH_EXTERNAL/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
