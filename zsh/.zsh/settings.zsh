e798292ebe03b6fa0373b34c8900b6b01eb38fc7
Paul Tan <pjxtan@gmail.com>
2022-06-10 18:07:18 -0400

    [zsh] Add mapping for edit-command-line

--- zsh/.zsh/settings.zsh
+++ zsh/.zsh/settings.zsh
@@ -8,3 +8,7 @@ setopt share_history
 # Prevent auto-removal of trailing slashes on dirs upon tab completion
 # (Really annoying with rsync in particular)
 setopt no_auto_remove_slash
+
+autoload -U edit-command-line
+zle -N edit-command-line
+bindkey '^x^e' edit-command-line
