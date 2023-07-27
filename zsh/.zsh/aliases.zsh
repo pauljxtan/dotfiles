91adb8648be4f49487d051edaf6cebc84404c1bd
Paul Tan <pjxtan@gmail.com>
2023-07-27 19:58:10 -0400

    [zsh] Update alias

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -22,5 +22,5 @@ alias rash='racket -l rash/repl'
 # Run mpv with minimal opts for SVP
 # https://www.svp-team.com/wiki/SVP:mpv
 alias mpvforsvp='mpv --no-config --input-ipc-server=/tmp/mpvsocket \
---hwdec=auto-copy --hwdec-codecs=all --hr-seek-framedrop=no \
+--hwdec=vaapi-copy --hwdec-codecs=all --hr-seek-framedrop=no \
 --no-resume-playback --opengl-early-flush=no'
